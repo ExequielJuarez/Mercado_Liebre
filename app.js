5996794825d0ff869af9bf65e7b5f7928d080756
ExequielJuarez <Exequieljuarezz154@gmail.com>
2023-11-13 18:52:41 -0300

    formulario

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const path = require('path')
-const app = express()
+const app = express();
+exports.app = app;
 
 app.use(express.static('public'));
 
 app.listen(3030,() => console.log('servidor corriendo'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname,'/views/home.html')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname,'/views/home.html')));
+
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname,'/views/register.html')));
+
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname,'/views/login.html')));
+
+
